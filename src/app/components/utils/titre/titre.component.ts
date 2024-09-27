4fd927e9baf0a996c37b633b17ecd705e37cd6ec
Bebel19 <belgnaoui.othman@gmail.com>
2024-09-27 17:01:15 +0200

    Added all pages component (empty), title and menue in components/utils

---
+++ src/app/components/utils/titre/titre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titre',
+  standalone: true,
+  imports: [],
+  templateUrl: './titre.component.html',
+  styleUrl: './titre.component.css'
+})
+export class TitreComponent {
+
+}
---
+++ src/app/pages/gcode/gcode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gcode',
+  standalone: true,
+  imports: [],
+  templateUrl: './gcode.component.html',
+  styleUrl: './gcode.component.css'
+})
+export class GcodeComponent {
+
+}
