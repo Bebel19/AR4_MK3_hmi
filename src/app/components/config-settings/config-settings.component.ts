b97f53d52c62631d5af4c748cdbcb992a1a1a601
Bebel19 <belgnaoui.othman@gmail.com>
2024-10-01 15:36:00 +0200

    Added the components replicating the original python HMI.

---
+++ src/app/components/config-settings/config-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './config-settings.component.html',
+  styleUrl: './config-settings.component.css'
+})
+export class ConfigSettingsComponent {
+
+}
---
+++ src/app/components/main-controls/main-controls.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-controls.component.html',
+  styleUrl: './main-controls.component.css'
+})
+export class MainControlsComponent {
+
+}
