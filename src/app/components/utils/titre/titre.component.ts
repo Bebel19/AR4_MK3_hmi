774755db1f008ebf30fa1776e58d2ca9291a7c75
Bebel19 <belgnaoui.othman@gmail.com>
2024-10-02 02:01:58 +0200

    Added logo on the title component (components/utils)

--- src/app/components/utils/titre/titre.component.ts
+++ src/app/components/utils/titre/titre.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {NgClass, NgOptimizedImage} from "@angular/common";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-titre',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatToolbar,
+    NgClass
   ],
   templateUrl: './titre.component.html',
   styleUrl: './titre.component.css'
 })
 export class TitreComponent {
+  isConnected: boolean | undefined; //TODO
 
 }
